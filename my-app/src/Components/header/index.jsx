9a334f23061c6bc0bc188c2bf45cfd5f28882f87
ChAvetisyan <ch.avetisyan@gmail.com>
2023-01-06 21:27:51 +0400

    hw1-060123

---
+++ my-app/src/Components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+
+class Header extends Component{
+    render(){
+        return <>
+        <p>Hello, React!</p>
+        </>
+    }
+}
+
+export default Header;
